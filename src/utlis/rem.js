fcc090de0da4618da021d8f0dd5cbc917305c367
himself65 <himself65@outlook.com>
2019-11-21 19:54:52 +0800

    feat: init frontend scaffold

--- src/utlis/rem.js
+++
@@ -1,9 +0,0 @@
-const baseSize = 32
-const setHtmlSize = () => {
-  const scale = document.documentElement.clientWidth / 750
-  document.documentElement.style.fontSize = baseSize * Math.min(scale, 2) + 'px'
-}
-setHtmlSize()
-window.onresize = function() {
-  setHtmlSize()
-}
