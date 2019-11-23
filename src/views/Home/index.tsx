64f3d54b94b453e8c35d4941567c3547331e6686
himself65 <himself65@outlook.com>
2019-11-23 21:55:50 +0800

    feat: finished header part

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,5 +1,13 @@
 import React, { Fragment } from 'react'
 import Header from '~component/Header'
+import { Fabric, Stack, mergeStyleSets, PrimaryButton, Text } from 'office-ui-fabric-react'
+
+const classNames = mergeStyleSets({
+  learnMoreButton: {
+    transform: 'translate(0, -50%)', // tip: 元素向上移动50%
+    height: '5rem'
+  }
+})
 
 const HomePage: React.FC = () => {
   // fixme: unfinished page
@@ -10,6 +18,24 @@ const HomePage: React.FC = () => {
         { name: 'WhitePaper', url: '/contact' },
         { name: 'Follow us', url: '/contact' }
       ]}/>
+      <Fabric>
+        <Stack>
+          <Stack.Item align='center'>
+            <PrimaryButton className={classNames.learnMoreButton}>
+              <div>
+                <Text variant='xLarge'>Cdot</Text>
+                <br/>
+                <Text variant='large'>Learn More</Text>
+              </div>
+            </PrimaryButton>
+          </Stack.Item>
+          <Stack.Item align='center'>
+            <Text variant='xxLargePlus'>
+              A General-Purpose Hub for Ledger Interoperation
+            </Text>
+          </Stack.Item>
+        </Stack>
+      </Fabric>
     </Fragment>
   )
 }
