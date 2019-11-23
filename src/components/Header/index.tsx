46a28926cc1a990f7209e59d38da2b948ca8bca3
himself65 <himself65@outlook.com>
2019-11-23 19:00:42 +0800

    feat: finished header commandBar items

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { mergeStyleSets } from 'office-ui-fabric-react'
+
+import './index.less'
+
+export interface HeaderProps {
+  items: {
+    name: string
+    url?: string
+  }[]
+}
+
+const classNames = mergeStyleSets({
+  header: {
+    height: '900px',
+    background: 'no-repeat center center',
+    backgroundImage: 'url(/assets/images/banner_img_bg_default.png)',
+    backgroundAttachment: 'scroll',
+    backgroundSize: 'cover'
+  },
+  commandBar: {
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row-reverse',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100px',
+    background: 'transparent',
+    color: 'white'
+  },
+  commandBarItems: {
+    float: 'right',
+    marginRight: '1rem',
+    paddingRight: '3rem',
+    left: '10px',
+    fontSize: '24px',
+    fontWeight: 'lighter',
+    display: 'flex',
+    flexDirection: 'row'
+  }
+})
+
+const Header: React.FC<HeaderProps> = ({ items }) => {
+  return (
+    <div className={classNames.header}>
+      <div className={classNames.commandBar}>
+        <div className={classNames.commandBarItems}>
+          {items.map((item, index) => (<span style={{
+            paddingLeft: index ? '2rem' : 0,
+            paddingRight: index === (items.length - 1) ? '2rem' : 0 // 最后一个 element 给 切换语言按钮 padding
+          }} key={item.name}>{item.name}</span>))}
+          <span>EN</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
