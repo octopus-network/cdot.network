5e26e59487aeba9b758e0ce0f5581e32281017bd
himself65 <himself65@outlook.com>
2019-11-24 11:29:03 +0800

    feat: finished IntroduceCard and top-3 cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { initializeIcons } from '@uifabric/icons'
 import importedComponent from 'react-imported-component'
 import { Route, Switch } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
+
+// tip: load fabric icons
+initializeIcons()
 
 const asyncComponentFactory = (resolve: () => Promise<React.ComponentType<any> | { default: React.ComponentType<any> }>) =>
   importedComponent(resolve)
