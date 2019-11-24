8ce473b73fa2dc80adff72a69fb1e119b6b9671f
himself65 <himself65@outlook.com>
2019-11-24 22:58:39 +0800

    feat: init NewsCard

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react'
 import Header from '~component/Header'
-import { Fabric, Stack, mergeStyleSets, PrimaryButton, Text } from 'office-ui-fabric-react'
+import { Fabric, Stack, mergeStyleSets, PrimaryButton, Text, Image } from 'office-ui-fabric-react'
 import IntroduceCard from '~component/IntroduceCard'
+import NewsCard from '~component/NewsCard'
 
 const classNames = mergeStyleSets({
   learnMoreButton: {
@@ -35,7 +36,7 @@ const HomePage: React.FC = () => {
               A General-Purpose Hub for Ledger Interoperation
             </Text>
           </Stack.Item>
-          <Stack.Item align='center'>
+          <Stack.Item styles={{ root: { paddingBottom: '5rem' } }} align='center'>
             <Stack tokens={{
               childrenGap: '2rem'
             }} horizontal>
@@ -57,6 +58,79 @@ const HomePage: React.FC = () => {
                 iconUrl='assets/images/content1_icon_right_default.png'
                 to=''
               />
+            </Stack>
+          </Stack.Item>
+        </Stack>
+        <Stack styles={{
+          root: {
+            backgroundColor: '#271245'
+          }
+        }}>
+          <Stack.Item styles={{
+            root: {
+              paddingTop: '2rem'
+            }
+          }} align='center'>
+            <Text styles={{ root: { color: '#D5D5D5' } }} variant='xxLargePlus'>
+              How You Can Get Involved
+            </Text>
+          </Stack.Item>
+          <Stack.Item
+            styles={{
+              root: {
+                paddingTop: '3rem',
+                paddingBottom: '5rem'
+              }
+            }}
+            align='center'
+          >
+            <Image src='/assets/images/content3_icon_bg_default.png'/>
+          </Stack.Item>
+        </Stack>
+        <Stack>
+          <Stack.Item
+            styles={{
+              root: {
+                paddingTop: '3rem',
+                paddingBottom: '5rem'
+              }
+            }}
+            align='center'
+          >
+            <Text variant='xxLargePlus'>Latest News</Text>
+          </Stack.Item>
+          <Stack.Item align='center'>
+            <Stack tokens={{
+              childrenGap: '2rem'
+            }} horizontal>
+              <NewsCard
+                titleImgUrl='assets/images/content3_img_fill1_default.png'
+                title={'Describe the Polkadot architecture in ten thousand words'}
+                description={'Teacher liu yi deeply discussed the current situation of DApp developmentthree expansion……'}
+                author={'Louis Liu'}
+                date={'2019-04-30 11:34'}
+                to=''/>
+              <NewsCard
+                titleImgUrl='assets/images/content3_img_fill2_default.png'
+                title={'Pay attention to "application governance," which is DApp\'s Achilles heel'}
+                description={'Let\'s start with the meaning of blockchain. One of the biggest benefits of blockchain is that it reduces……'}
+                author={'Louis Liu'}
+                date={'2019-04-30 11:34'}
+                to=''/>
+              <NewsCard
+                titleImgUrl='assets/images/content3_img_fill3_default.png'
+                title={'A brief history of Web3 ideas -- to real blockchain entrepreneurs'}
+                description={'From the open society to the cypherpunk movement, from bitcoin to ethereum to Polkadot, the……'}
+                author={'Louis Liu'}
+                date={'2019-04-30 11:34'}
+                to=''/>
+              <NewsCard
+                titleImgUrl='assets/images/content3_img_fill4_default.png'
+                title={'Encryption protocol governance: make good use of the force of evolution……'}
+                description={'We Chinese coin circle of retail investors used to laugh at themselves, leek, move bricks, helpless pain and so on……'}
+                author={'Louis Liu'}
+                date={'2019-04-30 11:34'}
+                to=''/>
             </Stack>
           </Stack.Item>
         </Stack>
