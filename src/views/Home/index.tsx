f8b30ec58dadda0e23fe19114838dde788088281
himself65 <himself65@outlook.com>
2019-11-25 15:22:04 +0800

    fix: width on NewsCardList

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -99,38 +99,61 @@ const HomePage: React.FC = () => {
           >
             <Text variant='xxLargePlus'>Latest News</Text>
           </Stack.Item>
-          <Stack.Item align='center'>
-            <Stack tokens={{
-              childrenGap: '2rem'
-            }} horizontal>
-              <NewsCard
-                titleImgUrl='assets/images/content3_img_fill1_default.png'
-                title={'Describe the Polkadot architecture in ten thousand words'}
-                description={'Teacher liu yi deeply discussed the current situation of DApp developmentthree expansion……'}
-                author={'Louis Liu'}
-                date={'2019-04-30 11:34'}
-                to=''/>
-              <NewsCard
-                titleImgUrl='assets/images/content3_img_fill2_default.png'
-                title={'Pay attention to "application governance," which is DApp\'s Achilles heel'}
-                description={'Let\'s start with the meaning of blockchain. One of the biggest benefits of blockchain is that it reduces……'}
-                author={'Louis Liu'}
-                date={'2019-04-30 11:34'}
-                to=''/>
-              <NewsCard
-                titleImgUrl='assets/images/content3_img_fill3_default.png'
-                title={'A brief history of Web3 ideas -- to real blockchain entrepreneurs'}
-                description={'From the open society to the cypherpunk movement, from bitcoin to ethereum to Polkadot, the……'}
-                author={'Louis Liu'}
-                date={'2019-04-30 11:34'}
-                to=''/>
-              <NewsCard
-                titleImgUrl='assets/images/content3_img_fill4_default.png'
-                title={'Encryption protocol governance: make good use of the force of evolution……'}
-                description={'We Chinese coin circle of retail investors used to laugh at themselves, leek, move bricks, helpless pain and so on……'}
-                author={'Louis Liu'}
-                date={'2019-04-30 11:34'}
-                to=''/>
+          <Stack.Item
+            styles={{
+              root: {
+                width: '100%'
+              }
+            }}
+            align='center'
+          >
+            <Stack
+              tokens={{
+                childrenGap: '2rem'
+              }}
+              styles={{
+                root: {
+                  overflowX: 'auto'
+                }
+              }}
+              horizontal
+            >
+              <Stack.Item>
+                <NewsCard
+                  titleImgUrl='assets/images/content3_img_fill1_default.png'
+                  title={'Describe the Polkadot architecture in ten thousand words'}
+                  description={'Teacher liu yi deeply discussed the current situation of DApp developmentthree expansion……'}
+                  author={'Louis Liu'}
+                  date={'2019-04-30 11:34'}
+                  to=''/>
+              </Stack.Item>
+              <Stack.Item>
+                <NewsCard
+                  titleImgUrl='assets/images/content3_img_fill2_default.png'
+                  title={'Pay attention to "application governance," which is DApp\'s Achilles heel'}
+                  description={'Let\'s start with the meaning of blockchain. One of the biggest benefits of blockchain is that it reduces……'}
+                  author={'Louis Liu'}
+                  date={'2019-04-30 11:34'}
+                  to=''/>
+              </Stack.Item>
+              <Stack.Item>
+                <NewsCard
+                  titleImgUrl='assets/images/content3_img_fill3_default.png'
+                  title={'A brief history of Web3 ideas -- to real blockchain entrepreneurs'}
+                  description={'From the open society to the cypherpunk movement, from bitcoin to ethereum to Polkadot, the……'}
+                  author={'Louis Liu'}
+                  date={'2019-04-30 11:34'}
+                  to=''/>
+              </Stack.Item>
+              <Stack.Item>
+                <NewsCard
+                  titleImgUrl='assets/images/content3_img_fill4_default.png'
+                  title={'Encryption protocol governance: make good use of the force of evolution……'}
+                  description={'We Chinese coin circle of retail investors used to laugh at themselves, leek, move bricks, helpless pain and so on……'}
+                  author={'Louis Liu'}
+                  date={'2019-04-30 11:34'}
+                  to=''/>
+              </Stack.Item>
             </Stack>
           </Stack.Item>
         </Stack>
