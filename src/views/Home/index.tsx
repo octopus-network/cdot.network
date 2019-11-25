fa8739c4a3a6ba9cacbb5bfd04fac919e918db6b
Yuanchao Sun <yuanchao.sun@gmail.com>
2019-11-25 22:50:20 +0800

    replace article abstract
    todo: try to align text

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -130,8 +130,8 @@ const HomePage: React.FC = () => {
               <Stack.Item>
                 <NewsCard
                   titleImgUrl='assets/images/content3_img_fill1_default.png'
-                  title={'Describe the Polkadot architecture in ten thousand words'}
-                  description={'Teacher liu yi deeply discussed the current situation of DApp developmentthree expansion……'}
+                  title={'A thorough interpretation on the architecture of Polkadot.'}
+                  description={'In this article, Louis Liu shared his insights on the future trend of Appchains.'}
                   author={'Louis Liu'}
                   date={'2019-04-30 11:34'}
                   to=''/>
@@ -139,8 +139,8 @@ const HomePage: React.FC = () => {
               <Stack.Item>
                 <NewsCard
                   titleImgUrl='assets/images/content3_img_fill2_default.png'
-                  title={'Pay attention to "application governance," which is DApp\'s Achilles heel'}
-                  description={'Let\'s start with the meaning of blockchain. One of the biggest benefits of blockchain is that it reduces……'}
+                  title={'Insight: ‘The governance of applications’, that’s the Achilles\' Heel of Dapp.'}
+                  description={'Talking about blockchain, the most noticeable merits it brings about is cutting down the transaction costs as it achieves trust minimal.'}
                   author={'Louis Liu'}
                   date={'2019-04-30 11:34'}
                   to=''/>
@@ -148,8 +148,8 @@ const HomePage: React.FC = () => {
               <Stack.Item>
                 <NewsCard
                   titleImgUrl='assets/images/content3_img_fill3_default.png'
-                  title={'A brief history of Web3 ideas -- to real blockchain entrepreneurs'}
-                  description={'From the open society to the cypherpunk movement, from bitcoin to ethereum to Polkadot, the……'}
+                  title={'A brief history about Web3 – to all the genuine blockchain startups.'}
+                  description={'From open society to cypher punk, from Bitcoin to Ethereum then to Polkadot, the development of Web3 has always on the track of its vision. What should startups in blockchain industry be looking for?'}
                   author={'Louis Liu'}
                   date={'2019-04-30 11:34'}
                   to=''/>
@@ -157,8 +157,8 @@ const HomePage: React.FC = () => {
               <Stack.Item>
                 <NewsCard
                   titleImgUrl='assets/images/content3_img_fill4_default.png'
-                  title={'Encryption protocol governance: make good use of the force of evolution……'}
-                  description={'We Chinese coin circle of retail investors used to laugh at themselves, leek, move bricks, helpless pain and so on……'}
+                  title={'The key to protocol governance: achieving network effect by making good use of the power of evolution.'}
+                  description={'Self-mockery has always been popular among Chinese retail investors. However, these retail investors are the ones that blockchain projects most care about.'}
                   author={'Louis Liu'}
                   date={'2019-04-30 11:34'}
                   to=''/>
