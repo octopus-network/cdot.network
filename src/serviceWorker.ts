888a3c21f13d2b99b43dad9edaa3fab7b6688b0c
himself65 <himself65@outlook.com>
2019-12-06 22:30:55 +0800

    fix: swUrl

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -121,8 +121,8 @@ export function register (config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
-      const swUrl = '/service-worker.js'
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+      // const swUrl = '/service-worker.js'
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
