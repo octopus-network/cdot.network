3dfcf17729f6c47dd11b020e0c4a227063379768
himself65 <himself65@outlook.com>
2019-12-14 11:58:19 +0800

    feat: finished FootBar

--- src/components/FootBar/index.tsx
+++ src/components/FootBar/index.tsx
@@ -1,84 +1,114 @@
-import React from 'react'
-import { Stack, Image, Text, IStackItemTokens } from 'office-ui-fabric-react'
+import React, { useEffect, useState } from 'react'
+import { Stack, Text, Image, Separator, mergeStyleSets, mergeStyles } from 'office-ui-fabric-react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faWeixin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-const childStackTokens: IStackItemTokens = {
-  margin: '7rem 10rem 3rem 10rem'
-}
+const classNames = mergeStyleSets({
+  root: {
+    height: '200px'
+  },
+  banner: {
+    background: 'no-repeat center center',
+    backgroundColor: 'black',
+    backgroundAttachment: 'scroll',
+    backgroundSize: 'cover',
+    backgroundImage: 'url(/assets/images/background/footer.png)',
+    selectors: {
+      '@media(min-width: 1024px)': {
+        backgroundImage: 'url(/assets/images/background/footer@2x.png)'
+      }
+    }
+  },
+  left: {
+    marginLeft: '6rem',
+    selectors: {
+      '@media(max-width: 1024px)': {
+        marginLeft: '1rem'
+      }
+    }
+  },
+  right: {
+    marginRight: '6rem',
+    selectors: {
+      '@media(max-width: 1024px)': {
+        marginRight: '1rem'
+      }
+    }
+  },
+  copyright: {
+    color: '#DBDBDB',
+    fontSize: '18px'
+  }
+})
 
 const FootBar: React.FC = () => {
+  const [width, setWidth] = useState(window.screen.width)
+  useEffect(() => {
+    const onResize = () => {
+      setWidth(window.screen.width)
+    }
+    window.addEventListener('resize', onResize)
+    return () => {
+      window.removeEventListener('resize', onResize)
+    }
+  }, [])
   return (
     <Stack
-      styles={{
-        root: {
-          backgroundColor: '#3C144A'
-        }
+      horizontal={width > 640}
+      horizontalAlign={width > 640 ? 'space-between' : 'center'}
+      verticalAlign='center'
+      tokens={{
+        childrenGap: width > 640 ? 0 : '2rem'
       }}
+      className={mergeStyles(classNames.root, classNames.banner)}
     >
-      <Stack.Item tokens={childStackTokens}>
+      <Stack.Item>
         <Stack
-          horizontalAlign='space-between'
-          styles={{
-            root: {
-              color: '#D0C3DF'
-            }
+          className={classNames.left}
+          tokens={{
+            childrenGap: '1rem'
           }}
           horizontal
+          horizontalAlign='center'
+          verticalAlign='center'
         >
-          <Image src='/assets/images/footer_img_logo_default.png'/>
-          <Stack
-            styles={{
+          <Stack.Item>
+            <Image width={109} height={38} src='/assets/images/logo.png'/>
+          </Stack.Item>
+          <Stack.Item>
+            <Separator styles={{
               root: {
-                float: 'right'
+                height: '36px'
               }
-            }}
-            tokens={{
-              childrenGap: '4rem'
-            }}
-          >
-            <Stack.Item>
-              <Text variant='superLarge'>Follow us</Text>
-            </Stack.Item>
-            <Stack.Item>
-              <Stack
-                tokens={{
-                  childrenGap: '3rem'
-                }}
-                wrap
-                horizontal
-                verticalAlign='center'
-              >
-                <Stack.Item>
-                  <FontAwesomeIcon size='3x' icon={faTwitter}/>
-                </Stack.Item>
-                <Stack.Item>
-                  <FontAwesomeIcon size='3x' icon={faGithub}/>
-                </Stack.Item>
-                <Stack.Item>
-                  <FontAwesomeIcon size='3x' icon={faWeixin}/>
-                </Stack.Item>
-                <Stack.Item>
-                  <FontAwesomeIcon size='3x' icon={faEnvelope}/>
-                </Stack.Item>
-              </Stack>
-            </Stack.Item>
-          </Stack>
+            }} vertical/>
+          </Stack.Item>
+          <Stack.Item>
+            <Text className={classNames.copyright}>
+              Copyright © 2019 cdot.network foundation. All rights reserved.
+            </Text>
+          </Stack.Item>
         </Stack>
       </Stack.Item>
-      <Stack.Item
-        styles={{
-          root: {
-            color: '#9D92A9',
-            padding: '3rem 0 0 0'
-          }
-        }}
-        tokens={childStackTokens}
-      >
-        <Text variant='xxLarge'>
-          Cdot Network Foundation
-        </Text>
+      <Stack.Item>
+        <Stack
+          className={classNames.right}
+          tokens={{ childrenGap: '3rem' }}
+          horizontal
+        >
+          <Stack.Item>
+            <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faTwitter}/>
+          </Stack.Item>
+          <Stack.Item>
+            <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faGithub}/>
+          </Stack.Item>
+          <Stack.Item>
+            <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faWeixin}/>
+          </Stack.Item>
+          <Stack.Item>
+            <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faEnvelope}/>
+          </Stack.Item>
+        </Stack>
       </Stack.Item>
     </Stack>
   )
