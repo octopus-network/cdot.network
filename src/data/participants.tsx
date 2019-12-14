be0a655f74a0e9f299337839a949acd7c1a03292
himself65 <himself65@outlook.com>
2019-12-14 22:46:56 +0800

    feat: finished all static icon

--- src/data/participants.tsx
+++ src/data/participants.tsx
@@ -72,7 +72,48 @@ export const participant2: Participant = {
         }}
       >
         <div>
-
+          <div style={{
+            backgroundColor: '#F9D63F',
+            height: '20px',
+            width: '20px',
+            borderRadius: '5px',
+            transform: 'rotate(-45deg)'
+          }}>
+            <div style={{
+              backgroundColor: '#F9D63F',
+              position: 'absolute',
+              left: '-12px',
+              height: '21px',
+              width: '10px',
+              borderRadius: '10px'
+            }}/>
+            <div style={{
+              backgroundColor: '#F9D63F',
+              position: 'absolute',
+              left: '22px',
+              height: '21px',
+              width: '10px',
+              borderRadius: '10px'
+            }}/>
+            <div style={{
+              backgroundColor: '#F9D63F',
+              position: 'absolute',
+              left: '6px',
+              width: '10px',
+              height: '43px',
+              borderBottomLeftRadius: '10px',
+              borderBottomRightRadius: '10px'
+            }}/>
+          </div>
+          <div style={{
+            backgroundColor: '#F9D63F',
+            position: 'relative',
+            borderRadius: '10px',
+            height: '7px',
+            width: '28px',
+            left: '-10px',
+            bottom: '-18px'
+          }}/>
         </div>
       </ParticipantCircle>
     )
@@ -89,6 +130,31 @@ export const participant3: Participant = {
           backgroundColor: '#2F1B1F'
         }}
       >
+        <div>
+          <div className='cdot-icon-relayers' style={{
+            width: '25px',
+            height: '25px'
+          }}>
+            <div style={{
+              position: 'absolute',
+              left: '-10px',
+              top: '-10px',
+              width: '20px',
+              height: '20px',
+              borderRadius: '5px',
+              backgroundColor: '#EC5758'
+            }}/>
+            <div style={{
+              position: 'absolute',
+              right: '-10px',
+              bottom: '-10px',
+              width: '20px',
+              height: '20px',
+              borderRadius: '5px',
+              backgroundColor: '#EC5758'
+            }}/>
+          </div>
+        </div>
       </ParticipantCircle>
     )
   }
@@ -104,6 +170,49 @@ export const participant4: Participant = {
           backgroundColor: '#232536'
         }}
       >
+        <div style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, 26px)',
+          gridTemplateRows: 'repeat(2, 26px)',
+          gridRowGap: '3px',
+          gridColumnGap: '3px'
+        }}>
+          <div style={{
+            backgroundColor: '#409EFF',
+            width: '26px',
+            height: '26px',
+            borderRadius: '5px'
+          }}/>
+          <div style={{
+            backgroundColor: '#0071C7',
+            width: '26px',
+            height: '26px',
+            borderRadius: '5px'
+          }}/>
+          <div style={{
+            backgroundColor: '#024B86',
+            width: '26px',
+            height: '26px',
+            borderRadius: '5px'
+          }}/>
+          <svg width='26px' height='26px'>
+            <polygon
+              strokeLinejoin='round' stroke='#459EFF'
+              strokeWidth='10'
+              fill='#459EFF'
+              points='5,5 15,5 5,15'
+            />
+            <polygon
+              style={{
+                rotate: '45deg'
+              }}
+              points='7,5 5,7 18,20 20,18'
+              strokeLinejoin='round'
+              stroke='#459EFF'
+              strokeWidth='7'
+              fill='#459EFF'/>
+          </svg>
+        </div>
       </ParticipantCircle>
     )
   }
