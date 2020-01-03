1ae36a6d8c3b8429d2197e6db0ad67a234eefd5b
himself65 <himself65@outlook.com>
2020-01-03 12:23:59 +0800

    feat: using async import

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import importedComponent from 'react-imported-component'
 import { initializeIcons } from '@uifabric/icons'
 import { Route, Switch } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
 
-import HomePage from '~view/Home'
-import ContactPage from '~view/Contact'
+const asyncComponentFactory = (resolve: () => Promise<React.ComponentType<any> | { default: React.ComponentType<any> }>) =>
+  importedComponent(resolve)
+
+const HomePage = asyncComponentFactory(() => import('~view/Home'))
+const ContactPage = asyncComponentFactory(() => import('~view/Contact'))
 
 // tip: load fabric icons
 initializeIcons()
