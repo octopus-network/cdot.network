c1174c89c7a6c0d8467060b331bd9bc041a8a39a
himself65 <himself65@outlook.com>
2020-01-29 13:23:50 +0800

    feat: add background on FootBar

--- src/components/FootBar/index.tsx
+++ src/components/FootBar/index.tsx
@@ -9,26 +9,38 @@ import './index.scss'
 const FootBar: React.FC = () => {
   return (
     <div className='cdot-foot-bar'>
-      <div className='cdot-foot-bar--left'>
-        <Image width={109} height={38} src='/assets/images/title/cdot-gray.svg'/>
-        <Separator
-          className='separator'
-          styles={{
-            root: {
-              height: '36px'
-            }
-          }}
-          vertical
-        />
-        <div className='copyright'>
+      <Image
+        className='cdot-foot-bar--banner'
+        width='100%'
+        height={150}
+        src={require('~asset/images/background/footer.png')}
+        srcSet={`
+          ${require('~asset/images/background/footer.png')} 1x,
+          ${require('~asset/images/background/footer@2x.png')} 2x
+        `}
+      />
+      <div className='cdot-foot-bar--inner'>
+        <div className='cdot-foot-bar--left'>
+          <Image width={109} height={38} src='/assets/images/title/cdot-gray.svg'/>
+          <Separator
+            className='separator'
+            styles={{
+              root: {
+                height: '36px'
+              }
+            }}
+            vertical
+          />
+          <div className='copyright'>
           Copyright © 2019 cdot.network foundation. All rights reserved.
+          </div>
         </div>
-      </div>
-      <div className='cdot-foot-bar--right'>
-        <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faTwitter}/>
-        <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faGithub}/>
-        <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faWeixin}/>
-        <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faEnvelope}/>
+        <div className='cdot-foot-bar--right'>
+          <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faTwitter}/>
+          <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faGithub}/>
+          <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faWeixin}/>
+          <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faEnvelope}/>
+        </div>
       </div>
     </div>
   )
