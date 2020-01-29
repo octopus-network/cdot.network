f7bfa72b820a4eba6cc51f7fe0d609b36cca6ceb
himself65 <himself65@outlook.com>
2020-01-29 12:32:57 +0800

    refactor: Component FootBar

--- src/components/FootBar/index.tsx
+++ src/components/FootBar/index.tsx
@@ -1,105 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { Stack, Text, Image, Separator, mergeStyleSets, mergeStyles } from 'office-ui-fabric-react'
+import React from 'react'
+import { Image, Separator } from 'office-ui-fabric-react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faWeixin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-const classNames = mergeStyleSets({
-  root: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    flexWrap: 'wrap',
-    height: '150px'
-  },
-  banner: {
-    background: 'no-repeat center center',
-    backgroundColor: 'black',
-    backgroundAttachment: 'scroll',
-    backgroundSize: 'cover',
-    backgroundImage: 'url(/assets/images/background/footer.png)',
-    selectors: {
-      '@media(min-width: 1024px)': {
-        backgroundImage: 'url(/assets/images/background/footer@2x.png)'
-      }
-    }
-  },
-  left: {
-    marginLeft: '101px',
-    paddingTop: '90px',
-    selectors: {
-      '@media(max-width: 1024px)': {
-        marginLeft: '1rem'
-      }
-    }
-  },
-  right: {
-    marginRight: '146px',
-    paddingTop: '89px',
-    selectors: {
-      '@media(max-width: 1024px)': {
-        marginRight: '1rem'
-      }
-    }
-  },
-  copyright: {
-    color: '#DBDBDB',
-    fontSize: '18px'
-  }
-})
+import './index.scss'
 
 const FootBar: React.FC = () => {
   return (
-    <div
-      className={mergeStyles(classNames.root, classNames.banner)}
-    >
-      <Stack.Item>
-        <Stack
-          className={classNames.left}
-          tokens={{
-            childrenGap: '17px'
+    <div className='cdot-foot-bar'>
+      <div className='cdot-foot-bar--left'>
+        <Image width={109} height={38} src='/assets/images/title/cdot-gray.svg'/>
+        <Separator
+          className='separator'
+          styles={{
+            root: {
+              height: '36px'
+            }
           }}
-          wrap={true}
-          horizontal
-          horizontalAlign='center'
-          verticalAlign='center'
-        >
-          <Stack.Item>
-            <Image width={109} height={38} src='/assets/images/title/cdot-gray.svg'/>
-          </Stack.Item>
-          <Stack.Item>
-            <Separator styles={{
-              root: {
-                height: '36px'
-              }
-            }} vertical/>
-          </Stack.Item>
-          <Stack.Item>
-            <Text className={classNames.copyright}>
-              Copyright © 2019 cdot.network foundation. All rights reserved.
-            </Text>
-          </Stack.Item>
-        </Stack>
-      </Stack.Item>
-      <Stack.Item>
-        <Stack
-          className={classNames.right}
-          tokens={{ childrenGap: '44px' }}
-          horizontal
-        >
-          <Stack.Item>
-            <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faTwitter}/>
-          </Stack.Item>
-          <Stack.Item>
-            <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faGithub}/>
-          </Stack.Item>
-          <Stack.Item>
-            <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faWeixin}/>
-          </Stack.Item>
-          <Stack.Item>
-            <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faEnvelope}/>
-          </Stack.Item>
-        </Stack>
-      </Stack.Item>
+          vertical
+        />
+        <div className='copyright'>
+          Copyright © 2019 cdot.network foundation. All rights reserved.
+        </div>
+      </div>
+      <div className='cdot-foot-bar--right'>
+        <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faTwitter}/>
+        <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faGithub}/>
+        <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faWeixin}/>
+        <FontAwesomeIcon color='rgb(136, 136, 136)' size='2x' icon={faEnvelope}/>
+      </div>
     </div>
   )
 }
