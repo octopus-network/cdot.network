a5c36056121124d57c0839883944fc5385a75e7c
himself65 <himself65@outlook.com>
2020-02-14 18:00:14 +0800

    deps: add mobx, mobx-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import importedComponent from 'react-imported-component'
+import { Provider } from 'mobx-react'
 import { initializeIcons } from '@uifabric/icons'
 import { Route, Switch } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from '~store'
 
 const asyncComponentFactory = (resolve: () => Promise<React.ComponentType<any> | { default: React.ComponentType<any> }>) =>
   importedComponent(resolve)
@@ -15,12 +17,14 @@ initializeIcons()
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/contact' component={ContactPage}/>
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/contact' component={ContactPage}/>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
